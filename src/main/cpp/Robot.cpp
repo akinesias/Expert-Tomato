9ea7fd641485064ec19ad54e91dbff1c74a38b35
Nick Schroeder <65873597+akinesias@users.noreply.github.com>
2021-06-18 13:17:25 -0500

    first robot code for no existing robot

---
+++ src/main/cpp/Robot.cpp
@@ -0,0 +1,33 @@
+
+#include "Robot.h"
+
+#include <iostream>
+
+#include <frc/smartdashboard/SmartDashboard.h>
+
+void Robot::RobotInit() {}
+
+void Robot::RobotPeriodic() {}
+
+void Robot::AutonomousInit() {}
+
+void Robot::AutonomousPeriodic() {}
+
+void Robot::TeleopInit() {}
+
+void Robot::TeleopPeriodic() {}
+
+void Robot::DisabledInit() {}
+
+void Robot::DisabledPeriodic() {}
+
+void Robot::TestInit() {}
+
+void Robot::TestPeriodic() {}
+
+//activates the robot
+#ifndef RUNNING_FRC_TESTS
+int main() {
+  return frc::StartRobot<Robot>();
+}
+#endif
